44dc995c41205bbc742d23ad3176ddd019f0831b
Max Rakitin <mrakitin@bnl.gov>
2024-03-25 15:05:37 -0400

    Working prototype of exporter with link

--- export_hdf.py
+++ export_hdf.py
@@ -1,6 +1,9 @@
+import datetime
 import os
+from pathlib import Path
 
 import h5py
+import numpy as np
 from tiled.client import from_uri
 
 
@@ -15,6 +18,72 @@ def get_filepath_from_run(run, stream_name):
     return filepath
 
 
+def export_tomo(run, export_dir=None, file_prefix=None, counter=0):
+    """Function to export bluesky run to NeXus file
+
+    Parameters:
+    -----------
+    run : bluesky run
+        the bluesky run to export to NeXus.
+    export_dir : str (optional)
+        the export directory for the resulting file.
+    file_prefix : str (optional)
+        the file prefix template for the resulting file.
+    """
+    start_doc = run.start
+    date = datetime.datetime.fromtimestamp(start_doc["time"])
+
+    if export_dir is None:
+        export_dir = "/nsls2/data/tst/legacy/mock-proposals/2024-1/pass-000000/exports"
+
+    if file_prefix is None:
+        file_prefix = "scan_{start[uid]}_{date.year:04d}-{date.month:02d}-{date.day:02d}-{counter:03d}.nxs"
+
+    rendered_file_name = file_prefix.format(start=start_doc, date=date, counter=counter)
+
+    nx_filepath = str(Path(export_dir) / Path(rendered_file_name))
+    print(f"{nx_filepath = }")
+
+    def get_dtype(value):
+        if isinstance(value, str):
+            return h5py.special_dtype(vlen=str)
+        if isinstance(value, float):
+            return np.float32
+        if isinstance(value, int):
+            return np.int32
+        return type(value)
+
+    with h5py.File(nx_filepath, "x") as h5_file:
+        entry_grp = h5_file.require_group("entry")
+        data_grp = entry_grp.require_group("data")
+
+        current_metadata_grp = h5_file.require_group("entry/instrument/detector")
+        metadata = {"uid": start_doc["uid"]}
+        for key, value in metadata.items():
+            if key not in current_metadata_grp:
+                dtype = get_dtype(value)
+                current_metadata_grp.create_dataset(key, data=value, dtype=dtype)
+
+        # External links:
+        manta_filepath = get_filepath_from_run(run, "manta_standard_det_stream")
+        panda_filepath = get_filepath_from_run(run, "panda3_standard_det_stream")
+        print(f"{manta_filepath = !r}\n{panda_filepath = !r}")
+
+        data_grp["manta_data"] = h5py.ExternalLink(manta_filepath, "entry/data/data")
+        data_grp["panda_data"] = h5py.ExternalLink(panda_filepath, "INENC1.VAL.Value")
+
+        # data = run.primary["data"][f"{det_name}_image"].read()
+        # frame_shape = data.shape[1:]
+        # data_grp.create_dataset(
+        #     "data",
+        #     data=data,
+        #     maxshape=(None, *frame_shape),
+        #     chunks=(1, *frame_shape),
+        #     dtype=data.dtype,
+        # )
+    return nx_filepath
+
+
 if __name__ == "__main__":
 
     tiled_client = from_uri(
@@ -26,7 +95,5 @@ if __name__ == "__main__":
     uid = "07dbb9ff-0dbd-46d6-86cb-5772529c0d02"
     run = tiled_client[uid]
 
-    manta_filepath = get_filepath_from_run(run, "manta_standard_det_stream")
-    panda_filepath = get_filepath_from_run(run, "panda3_standard_det_stream")
-
-    print(f"{manta_filepath = !r}\n{panda_filepath = !r}")
+    nx_filepath = export_tomo(run, export_dir=None, file_prefix=None, counter=4)
+    print(f"{nx_filepath = }")
