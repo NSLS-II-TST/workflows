7236ae4e02437a75c64c78019a1ea005836cc20d
Abigail Giles <agiles@bnl.gov>
2024-06-20 16:08:52 -0400

    Remove localhost tiled connection

--- data_validation.py
+++ data_validation.py
@@ -1,19 +1,13 @@
-import os
 from prefect import task, flow, get_run_logger
 import time as ttime
-from tiled.client import from_profile, from_uri
+from tiled.client import from_profile
 
 
 @task(retries=2, retry_delay_seconds=10)
 def read_all_streams(uid, beamline_acronym):
     logger = get_run_logger()
-    tiled_server_type = os.environ.get("TILED_SERVER_TYPE")
-    if tiled_server_type == "facility":
-        tiled_client = from_profile("nsls2")
-        run = tiled_client[beamline_acronym]["raw"][uid]
-    elif tiled_server_type == "local":
-        tiled_client = from_uri("http://localhost:8000")
-        run = tiled_client[uid]
+    tiled_client = from_profile("nsls2")
+    run = tiled_client[beamline_acronym]["raw"][uid]
     logger.info(f"Validating uid {run.start['uid']}")
     start_time = ttime.monotonic()
     for stream in run:
