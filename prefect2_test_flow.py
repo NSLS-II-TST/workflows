a38a186accf16800b0aa7f9f3256619d8d22221e
gbischof <bisc8233@gmail.com>
2025-04-14 14:44:11 -0400

    trying a decorator

--- prefect2_test_flow.py
+++ prefect2_test_flow.py
@@ -5,26 +5,44 @@ from prefect import flow, task, get_run_logger
 from prefect.blocks.notifications import SlackWebhook
 from prefect.context import FlowRunContext
 
+
 @task
 def print_task():
     print("Hello world!")
     logger = get_run_logger()
     logger.info("Hello world!")
 
+
+def slack(func):
+    def wrapper(*args, **kwargs):
+        flow_run_name = FlowRunContext.get().flow_run.dict().get("name")
+        slack_webhook = SlackWebhook.load("mon-prefect")
+
+        try:
+            result = func(*args, **kwargs)
+            slack_webhook.notify(
+                f":white_check_mark: Flow-run successful. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}```"
+            )
+            return result
+        except Exception as e:
+            tb = traceback.format_exception_only(e)
+            slack_webhook_block.notify(
+                f":bangbang: Flow-run failed. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
+            )
+            raise
+
+    return wrapper
+
+
 @flow
+@slack
 def hello_world():
-    try:
-        logger = get_run_logger()
-        logger.info("Starting flow")
-        print_task()
-        test_dict = dict()
-        test_dict['key']  # Trying to create a key error here.
-    except Exception as e:
-        tb = traceback.format_exception_only(e)
-        flow_run_name = FlowRunContext.get().flow_run.dict().get('name')
-        slack_webhook_block = SlackWebhook.load("mon-prefect")
-        slack_webhook_block.notify(f":bangbang: `flow-run` *{flow_run_name}* failed\n ```{tb[-1]}``` <@srx-prefect>")
-        raise
+    logger = get_run_logger()
+    logger.info("Starting flow")
+    print_task()
+    test_dict = dict()
+    test_dict["key"]  # Trying to create a key error here.
+
 
 if __name__ == "__main__":
     hello_world()
